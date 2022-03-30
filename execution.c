75470578631b0cfa5be5e7167dc8e2cc1d02e2b8
RAMAKRISHNAREDDY1234 <94432079+RAMAKRISHNAREDDY1234@users.noreply.github.com>
2022-03-30 15:57:40 +0530

    Add files via upload

---
+++ execution.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main()
+{
+pid_t;
+printf("child process id %d/n"get paid());
+else{
+printf("i am parent having id %d/n",get paid());
+}
+printf("commonm\n");
+}
+exec();
